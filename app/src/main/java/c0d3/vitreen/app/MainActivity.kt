4cfbb52dbdae8831779922a6d5047cd9999ee32a
1visible <redx.6345@gmail.com>
2021-03-20 19:54:40 +0100

    Initial commit

---
+++ app/src/main/java/c0d3/vitreen/app/MainActivity.kt
@@ -0,0 +1,19 @@
+package c0d3.vitreen.app
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+
+class MainActivity : AppCompatActivity() {
+    val TAG: String = "c0d3"
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Access a Cloud Firestore instance from your Activity
+        val db = Firebase.firestore
+
+    }
+}
---
+++ app/src/main/java/c0d3/vitreen/app/models/Roles.kt
@@ -0,0 +1,8 @@
+package c0d3.vitreen.app.models
+
+import java.util.*
+
+data class Roles(
+    val id: UUID = UUID.randomUUID(),
+    val name: String,
+)
