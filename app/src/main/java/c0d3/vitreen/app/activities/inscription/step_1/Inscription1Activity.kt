5eb070d7619ec390578d11c2d36793bfec6a55b9
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-22 21:45:58 +0100

    F08 : Activity created

---
+++ app/src/main/java/c0d3/vitreen/app/activities/inscription/step_1/Inscription1Activity.kt
@@ -0,0 +1,12 @@
+package c0d3.vitreen.app.activities.inscription.step_1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import c0d3.vitreen.app.R
+
+class Inscription1Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.inscription1)
+    }
+}
