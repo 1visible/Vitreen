5cf93feafd6e02866b34e87913dedd92610ddaca
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-22 22:25:15 +0100

    F08 : Sign In method

--- app/src/main/java/c0d3/vitreen/app/MainActivity.kt
+++ app/src/main/java/c0d3/vitreen/app/MainActivity.kt
@@ -1,18 +1,38 @@
 package c0d3.vitreen.app
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.ktx.Firebase
+import c0d3.vitreen.app.activities.inscription.step_1.Inscription1Activity
+import com.firebase.ui.auth.AuthUI
 
 class MainActivity : AppCompatActivity() {
     val TAG: String = "c0d3"
+    private lateinit var inscriptionButton: Button
+
+    companion object {
+        val RC_SIGN_IN: Int = 123
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        inscriptionButton = findViewById<Button>(R.id.InscriptionButton)
+        inscriptionButton.setOnClickListener {
+            startSignInActivity()
+        }
+    }
 
-        // Access a Cloud Firestore instance from your Activity
-        val db = Firebase.firestore
-
+    fun startSignInActivity() {
+        val providers = arrayListOf(
+            AuthUI.IdpConfig.EmailBuilder().build()
+        )
+        startActivityForResult(
+            AuthUI.getInstance()
+                .createSignInIntentBuilder()
+                .setAvailableProviders(providers)
+                .build(), RC_SIGN_IN
+        )
     }
 }
--- app/src/main/java/c0d3/vitreen/app/activities/inscription/step_1/Inscription1Activity.kt
+++ app/src/main/java/c0d3/vitreen/app/activities/inscription/step_1/Inscription1Activity.kt
@@ -3,10 +3,17 @@ package c0d3.vitreen.app.activities.inscription.step_1
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import c0d3.vitreen.app.R
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 
 class Inscription1Activity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.inscription1)
+        // Access a Cloud Firestore instance from your Activity
+        val db = Firebase.firestore
     }
 }
