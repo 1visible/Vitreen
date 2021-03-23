46845f1d84ad79af996397a041fdd0cc413c3762
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-23 11:24:13 +0100

    F09 : Get roles from db

---
+++ app/src/main/java/c0d3/vitreen/app/activities/inscription/step_2/Inscription2Activity.kt
@@ -0,0 +1,43 @@
+package c0d3.vitreen.app.activities.inscription.step_2
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.AutoCompleteTextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import c0d3.vitreen.app.R
+import c0d3.vitreen.app.models.Role
+import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
+
+class Inscription2Activity : AppCompatActivity() {
+
+    private lateinit var roles: ArrayList<Role>
+
+    private lateinit var textField: TextInputLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_inscription_step_2)
+        textField = findViewById<TextInputLayout>(R.id.menu)
+        roles = ArrayList<Role>()
+        recupRole()
+    }
+
+    private fun recupRole() {
+        val db = Firebase.firestore
+        db.collection("Role")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result) {
+                    roles.add(Role(document["name"] as String))
+                }
+                val items = roles.toList()
+                val adapter = ArrayAdapter(this, R.layout.list_item, items)
+                (textField.editText as? AutoCompleteTextView)?.setAdapter(adapter)
+            }
+    }
+
+}
--- app/src/main/java/c0d3/vitreen/app/models/Role.kt
+++ app/src/main/java/c0d3/vitreen/app/models/Role.kt
@@ -4,3 +4,4 @@ package c0d3.vitreen.app.models
 data class Role(
     val name: String,
 )
+
