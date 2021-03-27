1941f81360d93c2cb0f95f1de9ad422c256d7eec
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-27 21:08:51 +0100

    F11 : Sign in user

--- app/src/main/java/c0d3/vitreen/app/MainActivity.kt
+++ app/src/main/java/c0d3/vitreen/app/MainActivity.kt
@@ -3,18 +3,11 @@ package c0d3.vitreen.app
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import c0d3.vitreen.app.activities.connexion.ConnexionActivity
+import c0d3.vitreen.app.activities.connexion.ConnectionActivity
 import c0d3.vitreen.app.activities.inscription.step_1.Inscription1Activity
-import com.firebase.ui.auth.AuthUI
-import com.firebase.ui.auth.ErrorCodes
-import com.firebase.ui.auth.IdpResponse
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import io.grpc.okhttp.internal.framed.ErrorCode
-import kotlin.math.E
 
 class MainActivity : AppCompatActivity() {
     private lateinit var inscriptionButton: Button
@@ -36,7 +29,7 @@ class MainActivity : AppCompatActivity() {
         if (user == null) {
             connexionButton.text = R.string.connexion.toString()
             connexionButton.setOnClickListener {
-                startActivity(Intent(this, ConnexionActivity::class.java))
+                startActivity(Intent(this, ConnectionActivity::class.java))
             }
         }
     }
---
+++ app/src/main/java/c0d3/vitreen/app/activities/connexion/ConnectionActivity.kt
@@ -0,0 +1,50 @@
+package c0d3.vitreen.app.activities.connexion
+
+import android.content.Intent
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import c0d3.vitreen.app.Constantes
+import c0d3.vitreen.app.MainActivity
+import c0d3.vitreen.app.R
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import kotlin.math.sign
+
+class ConnectionActivity : AppCompatActivity() {
+
+    private lateinit var email: EditText
+    private lateinit var password: EditText
+    private lateinit var connexionButton: Button
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_connection)
+        email = findViewById<EditText>(R.id.email)
+        password = findViewById<EditText>(R.id.password)
+        connexionButton = findViewById<Button>(R.id.connexionButton)
+        connexionButton.setOnClickListener {
+            signInUser()
+        }
+
+    }
+
+    private fun signInUser() {
+        Firebase.auth
+            .signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    Log.d(Constantes.TAG, "Auth Succeed")
+                    startActivity(Intent(this, MainActivity::class.java))
+                } else {
+                    Log.w(Constantes.TAG, "Auth failed")
+                    Toast.makeText(this, "Auth failed", Toast.LENGTH_SHORT).show()
+                    email.text.clear()
+                    password.text.clear()
+                }
+            }
+    }
+}
