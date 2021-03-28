5e0cb2536c4dbd0f2c13916273322399aadf78ca
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-28 19:14:06 +0200

    F14 : remove access if user is not connected

--- app/src/main/java/c0d3/vitreen/app/activities/adverts/drop/DropAdvertActivity.kt
+++ app/src/main/java/c0d3/vitreen/app/activities/adverts/drop/DropAdvertActivity.kt
@@ -1,13 +1,26 @@
 package c0d3.vitreen.app.activities.adverts.drop
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import c0d3.vitreen.app.MainActivity
 import c0d3.vitreen.app.R
 import com.google.android.material.textfield.TextInputLayout
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class DropAdvertActivity : AppCompatActivity() {
 
-    private lateinit var category:TextInputLayout
+    private lateinit var category: TextInputLayout
+    private val user = Firebase.auth.currentUser
+
+    override fun onStart() {
+        super.onStart()
+        if (user == null) {
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
