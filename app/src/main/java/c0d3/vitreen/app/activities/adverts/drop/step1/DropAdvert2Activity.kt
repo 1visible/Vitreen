4b9761a7b59c21eae3514addbffe662b901d43f9
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-29 14:33:42 +0200

    F15 : Activity created

---
+++ app/src/main/java/c0d3/vitreen/app/activities/adverts/drop/step1/DropAdvert2Activity.kt
@@ -0,0 +1,12 @@
+package c0d3.vitreen.app.activities.adverts.drop.step1
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import c0d3.vitreen.app.R
+
+class DropAdvert2Activity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_drop_advert_2)
+    }
+}
