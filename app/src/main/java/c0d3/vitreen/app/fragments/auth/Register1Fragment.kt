7096b74d25f7dcda401948fc3baea3b75e27906d
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-31 11:25:22 +0200

    F16 : anonymous user removed

--- app/src/main/java/c0d3/vitreen/app/fragments/auth/Register1Fragment.kt
+++ app/src/main/java/c0d3/vitreen/app/fragments/auth/Register1Fragment.kt
@@ -1,5 +1,6 @@
 package c0d3.vitreen.app.fragments.auth
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -7,11 +8,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 import c0d3.vitreen.app.R
+import c0d3.vitreen.app.activities.inscription.step_2.Inscription2Activity
 import c0d3.vitreen.app.utils.ChildFragment
+import c0d3.vitreen.app.utils.Constants
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.fragment_register1.*
+import java.util.*
 
 class Register1Fragment : ChildFragment() {
 
@@ -37,7 +41,11 @@ class Register1Fragment : ChildFragment() {
             ) {
 
                 if (password.text.toString().equals(password_confirmation.text.toString())) {
-                    linkAnonymousToCredential()
+                    if ((user != null) && (user.isAnonymous)) {
+                        linkAnonymousToCredential()
+                    } else if (user == null) {
+                        registerUser()
+                    }
                 } else {
                     password.text.clear()
                     password_confirmation.text.clear()
@@ -56,24 +64,72 @@ class Register1Fragment : ChildFragment() {
                 ).show()
             }
         }
+        connectionButton.setOnClickListener {
+            removeUser()
+        }
+    }
+
+    private fun registerUser() {
+        auth
+            .createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    parentFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.nav_host_fragment,
+                            Register2Fragment.newInstance(email.text.toString())
+                        )
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.ErrorMessage),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    //pour l'instant ne redirige nulle part, on va juste afficher un toast disant que l'inscription a échoué
+                }
+            }
     }
 
     private fun linkAnonymousToCredential() {
-        if ((user != null) && (user.isAnonymous)) {
-            val credential =
-                EmailAuthProvider.getCredential(email.text.toString(), password.text.toString())
-            user!!
-                .linkWithCredential(credential)
+
+        val credential =
+            EmailAuthProvider.getCredential(email.text.toString(), password.text.toString())
+        user!!
+            .linkWithCredential(credential)
+            .addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    parentFragmentManager
+                        .beginTransaction()
+                        .replace(
+                            R.id.nav_host_fragment,
+                            Register2Fragment.newInstance(email.text.toString())
+                        )
+                        .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                        .commit()
+                } else {
+                    Toast.makeText(
+                        requireContext(),
+                        getString(R.string.ErrorMessage),
+                        Toast.LENGTH_SHORT
+                    ).show()
+                }
+            }
+    }
+
+    //Déplacer le code et exécuter au moment de la connexion du user
+    private fun removeUser() {
+        if ((user != null) && (user!!.isAnonymous)) {
+            val credential = EmailAuthProvider.getCredential(
+                "${UUID.randomUUID().toString()}@exemple.com",
+                "xu\$dqùdlqkfgo@^`4521"
+            )
+            user!!.linkWithCredential(credential)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
-                        parentFragmentManager
-                            .beginTransaction()
-                            .replace(
-                                R.id.nav_host_fragment,
-                                Register2Fragment.newInstance(email.text.toString())
-                            )
-                            .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                            .commit()
+                        user!!.delete()
                     } else {
                         Toast.makeText(
                             requireContext(),
