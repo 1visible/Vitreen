4794c93f1a49790e3d13bd409e87cd51a9c62633
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-03-31 10:21:54 +0200

    F16 : if user is anonymous redirect to register

--- app/src/main/java/c0d3/vitreen/app/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/c0d3/vitreen/app/fragments/profile/ProfileFragment.kt
@@ -2,12 +2,36 @@ package c0d3.vitreen.app.fragments.profile
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import c0d3.vitreen.app.R
 import c0d3.vitreen.app.activities.MainActivity
+import c0d3.vitreen.app.fragments.auth.Register1Fragment
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_main.*
 
 class ProfileFragment : Fragment() {
+
+    private val db = Firebase.firestore
+    private val auth = Firebase.auth
+    private val user = auth.currentUser
+
+    override fun onStart() {
+        super.onStart()
+        if (user == null) {
+            Toast.makeText(requireContext(), getString(R.string.ErrorMessage), Toast.LENGTH_SHORT)
+                .show()
+        } else if (user.isAnonymous) {
+            parentFragmentManager
+                .beginTransaction()
+                .replace(R.id.nav_host_fragment, Register1Fragment.newInstance())
+                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
+                .commit()
+        }
+    }
 
     // TODO: Remove this if not needed
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -15,7 +39,11 @@ class ProfileFragment : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         return inflater.inflate(R.layout.fragment_profile, container, false)
     }
 
@@ -27,7 +55,10 @@ class ProfileFragment : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        (activity as MainActivity).setTopViewAttributes(getString(R.string.signup), R.drawable.bigicon_user)
+        (activity as MainActivity).setTopViewAttributes(
+            getString(R.string.signup),
+            R.drawable.bigicon_user
+        )
     }
 
     // TODO: Remove this if not needed
@@ -41,6 +72,11 @@ class ProfileFragment : Fragment() {
             // Put things here
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance(): ProfileFragment = ProfileFragment()
     }
 
 }
