ef2f54fd45a9b0123c77e67351ce7bc4d581d5ad
1visible <redx.6345@gmail.com>
2021-04-01 19:27:43 +0200

    T03: Login design + Login/Register logic update

--- app/src/main/java/c0d3/vitreen/app/fragments/auth/LoginFragment.kt
+++ app/src/main/java/c0d3/vitreen/app/fragments/auth/LoginFragment.kt
@@ -1,96 +1,82 @@
 package c0d3.vitreen.app.fragments.auth
 
-import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
+import androidx.navigation.fragment.findNavController
 import c0d3.vitreen.app.R
 import c0d3.vitreen.app.activities.MainActivity
 import c0d3.vitreen.app.fragments.home.HomeFragment
 import c0d3.vitreen.app.utils.ChildFragment
-import c0d3.vitreen.app.utils.Constants
 import com.google.firebase.auth.EmailAuthProvider
-import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_login.*
+import kotlinx.android.synthetic.main.fragment_login.editTextEmail
+import kotlinx.android.synthetic.main.fragment_login.editTextPassword
+import kotlinx.android.synthetic.main.fragment_register1.*
 import java.util.*
 
 class LoginFragment : ChildFragment() {
-
     private val auth = Firebase.auth
     private var user = auth.currentUser
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
-    // TODO: Remove this if not needed
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        connexionButton.setOnClickListener {
-            if ((!(email.text.toString().equals(""))) && (!(password.text.toString().equals("")))) {
-                if (user == null) {
+        buttonSubmitLogin.setOnClickListener {
+            if (editTextEmail.text.toString().trim() != "" &&
+                editTextPassword.text.toString().trim() != "") {
+
+                if (user == null)
                     signInUser()
-                } else if (user!!.isAnonymous) {
+                else if (user!!.isAnonymous)
                     removeAnonymousUser()
-                }
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    getString(R.string.emptyFields),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+
+            } else
+                Toast.makeText(requireContext(), getString(R.string.emptyFields), Toast.LENGTH_SHORT).show()
         }
+
+        buttonToRegister1.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_login_to_navigation_register1)
+        }
+
     }
 
     private fun removeAnonymousUser() {
-        if ((user != null) && (user!!.isAnonymous)) {
-            val credential = EmailAuthProvider.getCredential(
-                "${UUID.randomUUID().toString()}@exemple.com",
-                "xu\$dqùdlqkfgo@^`4521"
-            )
+        if (user != null && user!!.isAnonymous) {
+            val credential = EmailAuthProvider.getCredential("${UUID.randomUUID().toString()}@exemple.com", "xu\$dqùdlqkfgo@^`4521")
+
             user!!.linkWithCredential(credential)
                 .addOnCompleteListener { task ->
+
                     if (task.isSuccessful) {
                         user!!.delete()
                         user = null
                         println("------------------------------- utilisateur effacée")
                         signInUser()
-                    } else {
-                        Toast.makeText(
-                            requireContext(),
-                            getString(R.string.ErrorMessage),
-                            Toast.LENGTH_SHORT
-                        ).show()
-                    }
+                    } else
+                        Toast.makeText(requireContext(), getString(R.string.ErrorMessage), Toast.LENGTH_SHORT).show()
+
                 }
+
         }
     }
 
     private fun signInUser() {
         Firebase.auth
-            .signInWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .signInWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
-                    Log.d(Constants.TAG, getString(R.string.SignInSucceed))
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.SignInSucceed),
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
+                    Toast.makeText(requireContext(), getString(R.string.SignInSucceed), Toast.LENGTH_SHORT).show()
+                    // TODO: Remplacer par la navigation
                     (activity as MainActivity).setBottomNavMenuIcon(R.id.navigation_home)
                     parentFragmentManager
                         .beginTransaction()
@@ -98,19 +84,10 @@ class LoginFragment : ChildFragment() {
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                         .commit()
                 } else {
-                    Log.w(Constants.TAG, "Auth failed")
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.ErrorMessage),
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
-                    password.text.clear()
+                    Toast.makeText(requireContext(), getString(R.string.ErrorMessage), Toast.LENGTH_SHORT).show()
+                    editTextPassword.text.clear()
                 }
             }
     }
 
-    companion object {
-        fun newInstance(): LoginFragment = LoginFragment()
-    }
 }
--- app/src/main/java/c0d3/vitreen/app/fragments/auth/Register1Fragment.kt
+++ app/src/main/java/c0d3/vitreen/app/fragments/auth/Register1Fragment.kt
@@ -1,17 +1,13 @@
 package c0d3.vitreen.app.fragments.auth
 
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
-import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.findNavController
 import c0d3.vitreen.app.R
-import c0d3.vitreen.app.activities.MainActivity
-import c0d3.vitreen.app.fragments.home.HomeFragment
 import c0d3.vitreen.app.utils.ChildFragment
 import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.ktx.auth
@@ -20,105 +16,79 @@ import kotlinx.android.synthetic.main.fragment_register1.*
 
 class Register1Fragment : ChildFragment() {
     private val auth = Firebase.auth
-    private var user = auth.currentUser
+    private val user = auth.currentUser
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_register1, container, false)
     }
 
-    // TODO: Remove this if not needed
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        nextButton.setOnClickListener {
-            if ((!(email.text.toString().replace("\\s+", "")
-                    .equals(""))) && (!(password.text.toString().replace("\\s+", "")
-                    .equals(""))) && (!(password_confirmation.text.toString().replace("\\s+", "")
-                    .equals("")))
-            ) {
 
-                if (password.text.toString().equals(password_confirmation.text.toString())) {
-                    if ((user!!.isAnonymous)) {
-                        linkAnonymousToCredential()
-                    } else if (user == null) {
+        buttonToRegister2.setOnClickListener {
+            if (editTextEmail.text.toString().trim() != "" &&
+                editTextPassword.text.toString().trim() != "" &&
+                editTextPasswordConfirmation.text.toString().trim() != "") {
+
+                if (editTextPassword.text.toString() == editTextPasswordConfirmation.text.toString()) {
+                    if (user == null)
                         registerUser()
-                    }
+                    else if (user.isAnonymous)
+                        linkAnonymousToCredential()
                 } else {
-                    password.text.clear()
-                    password_confirmation.text.clear()
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.NoMatchPassword),
-                        Toast.LENGTH_SHORT
-                    )
-                        .show()
+                    editTextPassword.text.clear()
+                    editTextPasswordConfirmation.text.clear()
+                    Toast.makeText(requireContext(), getString(R.string.NoMatchPassword), Toast.LENGTH_SHORT).show()
                 }
-            } else {
-                Toast.makeText(
-                    requireContext(),
-                    getString(R.string.emptyFields),
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
+
+            } else
+                Toast.makeText(requireContext(), getString(R.string.emptyFields), Toast.LENGTH_SHORT).show()
         }
-        connectionButton.setOnClickListener {
-            parentFragmentManager
-                .beginTransaction()
-                .replace(R.id.nav_host_fragment, LoginFragment.newInstance())
-                .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
-                .commit()
+
+        buttonToLogin.setOnClickListener {
+            findNavController().navigate(R.id.action_navigation_register1_to_navigation_login)
         }
+
     }
 
     private fun registerUser() {
         auth
-            .createUserWithEmailAndPassword(email.text.toString(), password.text.toString())
+            .createUserWithEmailAndPassword(editTextEmail.text.toString(), editTextPassword.text.toString())
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    // TODO: Replace this with navigation
                     parentFragmentManager
                         .beginTransaction()
-                        .replace(
-                            R.id.nav_host_fragment,
-                            Register2Fragment.newInstance(email.text.toString())
-                        )
+                        .replace(R.id.nav_host_fragment,
+                            Register2Fragment.newInstance(editTextEmail.text.toString()))
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                         .commit()
                 } else {
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.ErrorMessage),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.makeText(requireContext(), getString(R.string.ErrorMessage), Toast.LENGTH_SHORT).show()
+                    // TODO: Gérer les erreurs
                     //pour l'instant ne redirige nulle part, on va juste afficher un toast disant que l'inscription a échoué
                 }
             }
     }
 
     private fun linkAnonymousToCredential() {
-
-        val credential =
-            EmailAuthProvider.getCredential(email.text.toString(), password.text.toString())
+        val credential = EmailAuthProvider.getCredential(editTextEmail.text.toString(), editTextPassword.text.toString())
         user!!
             .linkWithCredential(credential)
             .addOnCompleteListener { task ->
                 if (task.isSuccessful) {
+                    // TODO: Replace this with navigation
                     parentFragmentManager
                         .beginTransaction()
                         .replace(
                             R.id.nav_host_fragment,
-                            Register2Fragment.newInstance(email.text.toString())
+                            Register2Fragment.newInstance(editTextEmail.text.toString())
                         )
                         .setTransition(FragmentTransaction.TRANSIT_FRAGMENT_OPEN)
                         .commit()
                 } else {
-                    Toast.makeText(
-                        requireContext(),
-                        getString(R.string.ErrorMessage),
-                        Toast.LENGTH_SHORT
-                    ).show()
+                    Toast.makeText(requireContext(), getString(R.string.ErrorMessage), Toast.LENGTH_SHORT).show()
+                    // TODO: Gérer les erreurs
                 }
             }
     }
