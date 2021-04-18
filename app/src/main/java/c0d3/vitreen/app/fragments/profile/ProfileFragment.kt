28f8a4326e291f535555c5a8127ec85989d3f437
benjaminAd <56395035+benjaminAd@users.noreply.github.com>
2021-04-19 01:08:42 +0200

    F20 : Profil (#23)
    * F20 : get current user
    
    * F20 : user profil
    
    * F20 : profil with user's adverts
    
    * F20 : add modify button
    
    Co-authored-by: benjaminadolphe <adolphe906@gmal.com>

--- app/src/main/java/c0d3/vitreen/app/fragments/profile/ProfileFragment.kt
+++ app/src/main/java/c0d3/vitreen/app/fragments/profile/ProfileFragment.kt
@@ -3,19 +3,31 @@ package c0d3.vitreen.app.fragments.profile
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentTransaction
 import androidx.navigation.fragment.findNavController
 import c0d3.vitreen.app.R
 import c0d3.vitreen.app.activities.MainActivity
+import c0d3.vitreen.app.adapter.AdvertAdapter
+import c0d3.vitreen.app.fragments.home.AdvertFragment
 import c0d3.vitreen.app.fragments.home.HomeFragment
+import c0d3.vitreen.app.models.dto.UserDTO
+import c0d3.vitreen.app.models.mini.AdvertMini
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.fragment_home.*
 import kotlinx.android.synthetic.main.fragment_profile.*
 
 class ProfileFragment : Fragment() {
     private val db = Firebase.firestore
+    private val userDb = db.collection("Users")
+    private val locationDB = db.collection("locations")
+    private val advertDB = db.collection("Adverts")
+
     private val auth = Firebase.auth
     private val user = auth.currentUser
+
+    private var advertList = ArrayList<AdvertMini>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,9 +35,9 @@ class ProfileFragment : Fragment() {
     }
 
     override fun onCreateView(
-            inflater: LayoutInflater,
-            container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         if (user == null || user.isAnonymous)
             findNavController().navigate(R.id.action_navigation_profile_to_navigation_register1)
@@ -39,29 +51,94 @@ class ProfileFragment : Fragment() {
             signOutButton.visibility = View.VISIBLE
             signOutButton.setOnClickListener {
                 auth
-                        .signOut()
+                    .signOut()
                 (activity as MainActivity).setBottomNavMenuIcon(R.id.navigation_home)
                 parentFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.nav_host_fragment, HomeFragment.newInstance())
-                        .commit()
+                    .beginTransaction()
+                    .replace(R.id.nav_host_fragment, HomeFragment.newInstance())
+                    .commit()
             }
-            db
-                    .collection("Users")
-                    .whereEqualTo("emailAddress", user.email)
-                    .get()
-                    .addOnSuccessListener { documents ->
-                        if (documents.size() == 1) {
-                            for (document in documents) {
-                                niy.text = "${getString(R.string.welcomeUser)} ${document.get("fullname")}"
-                            }
-                        } else {
-                            println("--------------------------------documents size > 1")
+            userDb
+                .whereEqualTo("emailAddress", user.email)
+                .get()
+                .addOnSuccessListener { documents ->
+                    if (documents.size() == 1) {
+                        var userDTO: UserDTO? = null
+                        for (document in documents) {
+                            userDTO = UserDTO(
+                                document.id,
+                                document.get("fullname") as String,
+                                document.get("emailAddress") as String,
+                                document.get("phoneNumber") as String,
+                                document.get("contactByPhone") as Boolean,
+                                document.get("isProfessional") as Boolean,
+                                document.get("locationId") as String,
+                                document.get("companyName") as String?,
+                                document.get("siretNumber") as String?,
+                                document.get("advertsId") as ArrayList<String>?,
+                                document.get("favoriteAdvertsId") as java.util.ArrayList<String>?
+                            )
                         }
+                        if (userDTO != null) {
+                            println("-------------------${userDTO.locationId}")
+                            locationDB
+                                .document(userDTO.locationId)
+                                .get()
+                                .addOnSuccessListener {
+                                    profilFullName.text = userDTO.fullname
+                                    profilEmailAddress.text = userDTO.emailAddress
+                                    profilPhoneNumber.text = userDTO.phoneNumber
+                                    profilContactByPhone.text =
+                                        if (userDTO.contactByPhone) "contactez moi par téléphone" else "Contactez moi par mail"
+                                    profilLocation.text =
+                                        "${it.get("name") as String}(${it.get("zipCode") as Long?})"
+                                    profilIsProfessional.text =
+                                        if (userDTO.isProfessional) "Je suis un professionnel" else "Je ne suis pas un professionnel"
+                                    if (userDTO.isProfessional) {
+                                        profilCompanyName.visibility = View.VISIBLE
+                                        profilCompanyName.text = userDTO.companyName
+                                        profilSiret.visibility = View.VISIBLE
+                                        profilSiret.text = userDTO.siretNumber
+                                        profilStatsButton.visibility = View.VISIBLE
+                                    } else {
+                                        profilCompanyName.visibility = View.INVISIBLE
+                                        profilSiret.visibility = View.INVISIBLE
+                                        profilStatsButton.visibility = View.INVISIBLE
+                                    }
+                                    if ((userDTO.advertsId != null) && (userDTO.advertsId!!.size > 0)) {
+                                        profilRecyclerView.visibility = View.VISIBLE
+                                        val advertAdapter: AdvertAdapter =
+                                            AdvertAdapter { advert -> adapterOnClick(advert) }
+                                        profilRecyclerView.adapter = advertAdapter
+                                        userDTO.advertsId!!.forEach { advertId ->
+                                            advertDB
+                                                .document(advertId)
+                                                .get()
+                                                .addOnSuccessListener {
+                                                    advertList.add(
+                                                        AdvertMini(
+                                                            it.id,
+                                                            it.get("title") as String,
+                                                            it.get("description") as String,
+                                                            it.get("price") as Long
+                                                        )
+                                                    )
+                                                    if (advertList.size == userDTO.advertsId!!.size) {
+                                                        advertAdapter.submitList(advertList)
+                                                    }
+                                                }
+                                        }
+                                    }
+                                }
+                        }
+
+                    } else {
+                        println("--------------------------------documents size > 1")
                     }
-                    .addOnFailureListener {
-                        println("-------------------------problème")
-                    }
+                }
+                .addOnFailureListener {
+                    println("-------------------------problème")
+                }
         } else {
             signOutButton.visibility = View.INVISIBLE
         }
@@ -81,6 +158,11 @@ class ProfileFragment : Fragment() {
             // Put things here
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+    /* Opens Advert  when RecyclerView item is clicked. */
+    private fun adapterOnClick(advert: AdvertMini) {
+
     }
 
     companion object {
