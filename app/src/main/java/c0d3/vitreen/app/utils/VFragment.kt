6e991b612527d79654a8934bd8c79a198ad8138b
1visible <redx.6345@gmail.com>
2021-04-20 00:52:30 +0200

    T05: Added base fragment

--- app/src/main/java/c0d3/vitreen/app/utils/VFragment.kt
+++ app/src/main/java/c0d3/vitreen/app/utils/VFragment.kt
@@ -2,6 +2,10 @@ package c0d3.vitreen.app.utils
 
 import android.os.Bundle
 import android.view.*
+import android.widget.EditText
+import android.widget.Toast
+import androidx.annotation.*
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import c0d3.vitreen.app.activities.MainActivity
@@ -11,13 +15,13 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 abstract class VFragment(
-        private val layoutId: Int,
-        private val topIcon: Int,
-        private val topTitleId: Int = -1,
+        @LayoutRes private val layoutId: Int,
+        @DrawableRes private val topIcon: Int,
+        @StringRes private val topTitleId: Int = -1,
         private val hasOptionsMenu: Boolean = false,
-        private val topMenuId: Int = -1,
+        @MenuRes private val topMenuId: Int = -1,
         private val requireAuth: Boolean = false,
-        private val loginNavigationId: Int = -1
+        @IdRes private val loginNavigationId: Int = -1
 ) : Fragment() {
 
     val db = Firebase.firestore
@@ -30,8 +34,8 @@ abstract class VFragment(
     }
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        if (requireAuth && (user == null || user.isAnonymous))
-            findNavController().navigate(loginNavigationId)
+        if (requireAuth && (user == null || user!!.isAnonymous))
+            navigateTo(loginNavigationId)
         return inflater.inflate(layoutId, container, false)
     }
 
@@ -48,6 +52,23 @@ abstract class VFragment(
 
     fun highlightMenuIcon(id: Int) {
         (activity as MainActivity).highlightMenuIcon(id)
+    }
+
+    fun navigateTo(@IdRes destinationId: Int, vararg args: Pair<String, Any?>) {
+        val bundle = bundleOf(*args)
+        findNavController().navigate(destinationId, bundle)
+    }
+
+    fun isAnyInputEmpty(vararg editTexts: EditText?): Boolean {
+        editTexts.forEach { editText ->
+            if(editText?.text.toString().trim().isEmpty())
+                return true
+        }
+        return false
+    }
+
+    fun showError(@StringRes errorId: Int) {
+        Toast.makeText(context, getString(errorId), Toast.LENGTH_SHORT).show()
     }
 
 }
