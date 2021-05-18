40169e1d0e9935bd963adbe5774d79146df5ffe0
benjaminadolphe <adolphe906@gmal.com>
2021-05-19 01:20:42 +0200

    T08 : delete activity content

--- app/src/main/java/c0d3/vitreen/app/activities/MainActivity.kt
+++ app/src/main/java/c0d3/vitreen/app/activities/MainActivity.kt
@@ -27,7 +27,6 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.firestoreSettings
 import com.google.firebase.ktx.Firebase
-import kotlinx.android.synthetic.main.activity_content.*
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity(), FirebaseAuth.AuthStateListener {
